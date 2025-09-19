6890245fd21ba263877ce7fe8c60494df4d711a3
Paul Duncan <pabs@pablotron.org>
2025-09-19 19:56:47 -0400

    src/bin/cvss-score.rs: print severity, update documentation

--- src/bin/cvss-score.rs
+++ src/bin/cvss-score.rs
@@ -11,35 +11,37 @@
 //! Score CVSS v2 vector string:
 //!
 //! ```sh
-//! $ cvss-score "AV:N/AC:L/Au:N/C:C/I:C/A:C"
-//! 10.0
+//! $ cvss-score "AV:A/AC:H/Au:N/C:C/I:C/A:C"
+//! 6.8 MEDIUM
 //! ```
 //! Score CVSS v3 vector string:
 //!
 //! ```sh
-//! $ cvss-score "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H"
-//! 9.8
+//! cvss-score "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H"
+//! 9.8 CRITICAL
 //! ```
 //!
 //! Score CVSS v4 vector string:
 //!
 //! ```sh
-//! $ cvss-score "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:P/VC:H/VI:H/VA:H/SC:H/SI:H/SA:H"
-//! 9.4
+//! cvss-score "CVSS:4.0/AV:L/AC:H/AT:N/PR:N/UI:P/VC:L/VI:L/VA:L/SC:H/SI:H/SA:H"
+//! 5.2 MEDIUM
 //! ```
 //! 
 //! [cvss]: https://first.org/cvss/
 //!   "Common Vulnerability Scoring System (CVSS)"
 
 use std::env;
-use polycvss::{Err, Score, Vector};
+use polycvss::{Err, Score, Severity, Vector};
 
 fn main() -> Result<(), Err> {
   let args: Vec<String> = env::args().collect(); // get cli args
 
   if args.len() == 2 {
     let vec: Vector = args[1].parse()?; // parse string
-    println!("{}", Score::from(vec)); // print score
+    let score = Score::from(vec); // get score
+    let severity = Severity::from(score); // get severity
+    println!("{score} {severity}"); // print score and severity
   } else {
     let name = args.first().map_or("app", |s| s); // get app name
     eprintln!("Usage: {name} [VECTOR]"); // print usage
