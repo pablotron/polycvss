01cad88f617446e9c481f399d330135c2ce8c0aa
Paul Duncan <pabs@pablotron.org>
2025-11-15 09:34:31 -0500

    add examples/metrics.rs

--- examples/README.md
+++ examples/README.md
@@ -2,7 +2,9 @@
 
 poly
 
-- `cvss-score.rs`: Parse command line argument as vector, then print
+- `cvss-score.rs`: Parse command line argument as vector, then print the
   score and severity.
+- `metrics.rs`: Iterate over vector metrics and print the metric, metric
+  name, and metric group.
 - `severities.rs`: Print severity of several vector strings.
 - `versions.rs`: Print version of several vector strings.
---
+++ examples/metrics.rs
@@ -0,0 +1,29 @@
+//! Parse [CVSS][] vector string, then iterate over vector metrics and
+//! print the metric, metric name, and metric group.
+//!
+//! # Usage
+//!
+//! ```sh
+//! cargo run --example metrics
+//! ```
+//! 
+//! [cvss]: https://first.org/cvss/
+//!   "Common Vulnerability Scoring System (CVSS)"
+
+use polycvss::{Err, Group, Name, Vector};
+
+fn main() -> Result<(), Err> {
+	// vector string
+	let s = "CVSS:4.0/AV:L/AC:H/AT:N/PR:N/UI:P/VC:L/VI:L/VA:L/SC:H/SI:H/SA:H/CR:H/IR:H/E:A/S:N";
+
+  // parse vector string
+  let vector: Vector = s.parse()?;
+
+  for metric in vector {
+    let name = Name::from(metric); // get metric name
+    let group = Group::from(metric); // get metric group
+    println!("{metric} {name} {group}"); // print metric, name, and group
+  }
+
+  Ok(())
+}
