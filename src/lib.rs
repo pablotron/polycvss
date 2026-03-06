6beb7423ec22bac69fd27006e3615a324a36b071
Paul Duncan <pabs@pablotron.org>
2026-03-06 17:00:59 -0500

    src/lib.rs: TODO: remove stuff

--- src/lib.rs
+++ src/lib.rs
@@ -253,8 +253,7 @@
 // - README.md: intro: s/rust library to (.*)/$1 in Rust/
 // - README.md: intro: document explicit v3.0 vs v3.1 scoring
 // - README.md: badges
-// - gh action for tests, coverage (fuzzing and mutation?)
-// - coverage (tarpaulin --fail-under 95)
+// - gh actions: fuzzing and mutation?
 // - features to convert to/from json/xml representations
 //   see <https://www.first.org/cvss/data-representations>
 
